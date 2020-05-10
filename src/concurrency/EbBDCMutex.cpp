63e473c210fb585155f1c5f095a4aa244f91ae26
Dcnqukin <dcn965662867@hotmail.com>
2020-05-10 17:38:27 +0800

    EbBDC[2][add]:添加互斥量Mutex的封装

---
+++ src/concurrency/EbBDCMutex.cpp
@@ -0,0 +1,59 @@
+#include "EbBDCMutex.h"
+
+int UtilityMutex::tryLock()
+{
+	int result = -1;
+#ifdef _WIN32
+	result = TryEnterCriticalSection(&m_lock);
+	return result;
+#elif __linux__
+	return pthread_mutex_trylock(&(m_lock)) == 0 ? 1 : 0;
+#endif
+	return result;
+}
+
+int UtilityMutex::lock()
+{
+#ifdef _WIN32
+	EnterCriticalSection(&m_lock);
+#elif __linux__
+	pthread_mutex_lock(&m_lock);
+#endif
+	return 0;
+}
+
+int UtilityMutex::unlock()
+{
+#ifdef _WIN32
+	LeaveCriticalSection(&m_lock);
+#elif __linux__
+	pthread_mutex_unlock(&m_lock);
+#endif
+	return 0;
+}
+
+UtilityMutex::UtilityMutex()
+{
+#ifdef _WIN32
+	InitializeCriticalSection(&m_lock);
+#elif __linux__
+	pthread_mutexattr_t mattr;
+	// initialize an attribute to default value
+	int ret = pthread_mutexattr_init(&mattr);
+	if (ret == 0)
+	{
+		pthread_mutexattr_settype(&mattr, PTHREAD_MUTEX_RECURSIVE);
+	}
+	pthread_mutex_init(&m_lock, &mattr);
+	pthread_mutexattr_destory(&mattr);
+#endif
+}
+
+UtilityMutex::~UtilityMutex()
+{
+#ifdef _WIN32
+	DeleteCriticalSection(&m_lock);
+#elif __linux__
+	pthread_mutex_destory(&m_lock);
+#endif
+}
---
+++ src/concurrency/EbBDCMutex.h
@@ -0,0 +1,31 @@
+#ifndef EBBDCMUTEX_H
+#define EBBDCMUTEX_H
+
+#ifdef _WIN32
+#include <windows.h>
+#elif __linux__
+#include <pthread.h>
+#endif
+
+class UtilityMutex
+{
+public:
+	int tryLock();
+
+	int lock();
+
+	int unlock();
+
+	UtilityMutex();
+
+	~UtilityMutex();
+private:
+#ifdef _WIN32
+	CRITICAL_SECTION m_lock;
+#elif __linux__
+	pthread_mutex_t m_lock;
+#endif
+};
+
+
+#endif
